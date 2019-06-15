3bd171fa81756677141fecc19266837b429abbbb
Supamiu <contact@flavien-normand.fr>
2019-06-15 14:20:05 +0200

    chore: lint fix

--- src/model/actions/quality/byregots-brow.ts
+++ src/model/actions/quality/byregots-brow.ts
@@ -1,25 +1,25 @@
-import { Simulation } from '../../../simulation/simulation'
-import { Buff } from '../../buff.enum'
-import { CraftingJob } from '../../crafting-job.enum'
-import { SimulationFailCause } from '../../simulation-fail-cause.enum'
-import { QualityAction } from '../quality-action'
+import { Simulation } from '../../../simulation/simulation';
+import { Buff } from '../../buff.enum';
+import { CraftingJob } from '../../crafting-job.enum';
+import { SimulationFailCause } from '../../simulation-fail-cause.enum';
+import { QualityAction } from '../quality-action';
 
 export class ByregotsBrow extends QualityAction {
   execute(simulation: Simulation): void {
-    super.execute(simulation)
-    simulation.removeBuff(Buff.INNER_QUIET)
+    super.execute(simulation);
+    simulation.removeBuff(Buff.INNER_QUIET);
   }
 
   getBaseDurabilityCost(simulationState: Simulation): number {
-    return 10
+    return 10;
   }
 
   getLevelRequirement(): { job: CraftingJob; level: number } {
-    return { job: CraftingJob.ANY, level: 51 }
+    return { job: CraftingJob.ANY, level: 51 };
   }
 
   _canBeUsed(simulation: Simulation): boolean {
-    return simulation.hasBuff(Buff.INNER_QUIET) && simulation.getBuff(Buff.INNER_QUIET).stacks >= 2
+    return simulation.hasBuff(Buff.INNER_QUIET) && simulation.getBuff(Buff.INNER_QUIET).stacks >= 2;
   }
 
   getFailCause(
@@ -28,24 +28,24 @@ export class ByregotsBrow extends QualityAction {
     safeMode?: boolean
   ): SimulationFailCause | undefined {
     if (!simulationState.hasBuff(Buff.INNER_QUIET)) {
-      return SimulationFailCause.NO_INNER_QUIET
+      return SimulationFailCause.NO_INNER_QUIET;
     }
-    return super.getFailCause(simulationState, linear, safeMode)
+    return super.getFailCause(simulationState, linear, safeMode);
   }
 
   getBaseCPCost(simulationState: Simulation): number {
-    return 18
+    return 18;
   }
 
   getBaseSuccessRate(simulationState: Simulation): number {
-    return simulationState.crafterStats.specialist ? 100 : 70
+    return simulationState.crafterStats.specialist ? 100 : 70;
   }
 
   getPotency(simulation: Simulation): number {
-    return 150 + (simulation.getBuff(Buff.INNER_QUIET).stacks - 1) * 10
+    return 150 + (simulation.getBuff(Buff.INNER_QUIET).stacks - 1) * 10;
   }
 
   getIds(): number[] {
-    return [100120, 100121, 100122, 100123, 100124, 100125, 100126, 100127]
+    return [100120, 100121, 100122, 100123, 100124, 100125, 100126, 100127];
   }
 }
