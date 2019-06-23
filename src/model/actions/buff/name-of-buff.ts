925335ec8af2db3aeac391c4f238a03df9cd50de
Supamiu <contact@flavien-normand.fr>
2019-06-23 16:47:20 +0200

    fix: fixed a bug allowing you to use Name of XXX twice in a rotation

--- src/model/actions/buff/name-of-buff.ts
+++ src/model/actions/buff/name-of-buff.ts
@@ -3,14 +3,16 @@ import { Simulation } from '../../../simulation/simulation';
 import { Buff } from '../../buff.enum';
 
 export abstract class NameOfBuff extends BuffAction {
-
   _canBeUsed(simulation: Simulation): boolean {
-    return !simulation.hasBuff(Buff.NAME_OF_FIRE)
-      && !simulation.hasBuff(Buff.NAME_OF_LIGHTNING)
-      && !simulation.hasBuff(Buff.NAME_OF_WATER)
-      && !simulation.hasBuff(Buff.NAME_OF_EARTH)
-      && !simulation.hasBuff(Buff.NAME_OF_ICE)
-      && !simulation.hasBuff(Buff.NAME_OF_THE_WIND);
+    return (
+      !simulation.actions.some(action => action.is(NameOfBuff)) &&
+      !simulation.hasBuff(Buff.NAME_OF_FIRE) &&
+      !simulation.hasBuff(Buff.NAME_OF_LIGHTNING) &&
+      !simulation.hasBuff(Buff.NAME_OF_WATER) &&
+      !simulation.hasBuff(Buff.NAME_OF_EARTH) &&
+      !simulation.hasBuff(Buff.NAME_OF_ICE) &&
+      !simulation.hasBuff(Buff.NAME_OF_THE_WIND)
+    );
   }
 
   getBaseCPCost(simulationState: Simulation): number {
