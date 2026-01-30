fbc0837226a1eea41f5ad38dc87261838c77ac96
Jengines <jeromemeyer23@gmail.com>
2026-01-30 17:28:32 +0100

    feat(simulator): rename splendorous to relic tool (#82)
    ```
    feat: relic tool instead of splendorous
    
    For now and further, not only splendorous have other effects to simulation but all relic tools
    ```
    
    Co-authored-by: Jengines <jeromeyer23@gmail.com>

--- src/model/actions/other/delicate-synthesis.ts
+++ src/model/actions/other/delicate-synthesis.ts
@@ -69,7 +69,7 @@ export class DelicateSynthesis extends GeneralAction {
         conditionMod *= 0.5;
         break;
       case StepState.GOOD:
-        conditionMod *= simulation.crafterStats.splendorous ? 1.75 : 1.5;
+        conditionMod *= simulation.crafterStats.relicTool ? 1.75 : 1.5;
         break;
       default:
         break;
